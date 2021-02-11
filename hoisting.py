79880e41b15c8a245c66981f9bfb05d9083b6db0
Thomas Fuller <tjfuller@fb.com>
2021-02-11 15:37:49 -0800

    Hoisting

---
+++ hoisting.py
@@ -0,0 +1,7 @@
+print(does_this_work())
+
+def does_this_work():
+    return "No"
+
+print("Execution will not reach here, but if it did, the following line would print 'No'")
+print(does_this_work())
